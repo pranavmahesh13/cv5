b9f9b0fe1938b695b799808cb36dcc0149001db0
pranavmahesh13 <70573993+pranavmahesh13@users.noreply.github.com>
2020-09-15 18:58:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+setTimeout(function(){
+  document.querySelector("h1").innerHTML="Welcome ";
+}, 750)
+
+
+setTimeout(function(){
+}, 1500)
+setTimeout(function(){
+  document.querySelector("h1").innerHTML="To";
+}, 1500)
+setTimeout(function(){
+  document.querySelector("h1").innerHTML="My ";
+}, 2250)
+setTimeout(function(){
+  document.querySelector("h1").innerHTML="Webpage ";
+}, 3000)
+
+setTimeout(function(){
+  document.querySelector("h1").innerHTML="Welcome";
+}, 3750);
+
+
+setTimeout(function(){
+  document.querySelector(".btnn").addEventListener("click", function(){
+    document.querySelector(".myint").innerHTML="Please feel free to mail me or DM me about your impression on my website";
+  });
+});
